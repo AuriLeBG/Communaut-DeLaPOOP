38ab89fb4947c96d922e3bccfbce5b21092a1b0f
Tom <darquestom@gmail.com>
2023-12-07 19:06:45 +0100

    Create Question class

---
+++ Class/Question.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Class;
+
+class Question
+{
+    private string $question;
+    private bool $answer;
+    private string $image;
+    function __construct(String $titre){
+
+    }
+}
