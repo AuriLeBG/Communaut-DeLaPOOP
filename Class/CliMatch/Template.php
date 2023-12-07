f1d122f2cebaec1be801ad3f9aa8bd1090acca15
ValentinD62 <129511290+ValentinD62@users.noreply.github.com>
2023-12-07 23:12:41 +0100

    easter egg version v2

--- Class/CliMatch/Template.php
+++ Class/CliMatch/Template.php
@@ -13,6 +13,7 @@ class Template{
             <link rel="icon" href="">
             <link rel="stylesheet" href="../css/style.css">
             <script src="../javascript/script.js"></script>
+            <script src="../javascript/easter_egg.js"></script>
             <script src="https://kit.fontawesome.com/eea040aea1.js" crossorigin="anonymous"></script>
         </head>
         <body>
