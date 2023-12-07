67ad940a4a76d1175619b804baff350ea3665bc8
Margot <margot.colo@gmail.com>
2023-12-07 23:45:53 +0100

    css amend

--- pages/header.php
+++ pages/header.php
@@ -2,7 +2,6 @@
 
     <div id="menu">
         <a href="../index.php" id="titlelogo">
-            <img id="spat" src="">
             <div id="main-title">
                 <img id="logo" src="../image/logo.png">
                 CliMatch
