298e8c38800b3756c7bcffaaefa0cc14e0d57ae4
Margot <margot.colo@gmail.com>
2023-12-07 22:09:38 +0100

    header.php en plus propre (hein TOM)

--- pages/header.php
+++ pages/header.php
@@ -8,34 +8,6 @@
                 <i class="fa-solid fa-heart"></i>
             </div>
         </a>
-
-        <form method="post" action="recherche.php" id="form">
-            <div id="barrerecherche">
-
-            </div>
-        </form>
     </div>
 
-    <div id="liens">
-        <?php
-        // Si l'utilisateur est connecté
-        if(isset($_SESSION["login"])):
-        ?>
-            <div id="connecte">
-
-                <a href="logout.php">
-                    <div id="loginAdmin" class="lien">
-                        Logout <?php echo $_SESSION["login"];?>
-                    </div>
-                </a>
-            </div>
-
-        <?php else:?>
-            <a href="login.php">
-                <div id="loginAdmin" class="lien">
-                    Connexion
-                </div>
-            </a>
-        <?php endif?>
-    </div>
 </header>
