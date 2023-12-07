59025c01338273fadf7663e4e8de67837bf4ca3a
Tom <darquestom@gmail.com>
2023-12-07 19:18:19 +0100

    add template and autoloader

--- Class/Question.php
+++ Class/Question.php
@@ -5,10 +5,12 @@ namespace Class;
 class Question
 {
     private string $question;
-    private bool $answer;
+    private string $answer;
     private string $image;
     private string $explanation;
     function __construct(string $question, string $answer, string $image){
 
     }
+
+    function
 }
