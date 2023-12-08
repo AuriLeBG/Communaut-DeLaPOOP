0870572bfa5730977e99c102f88726bcb5e32c91
Margot <margot.colo@gmail.com>
2023-12-08 01:08:02 +0100

    css amend

--- pages/header.php
+++ pages/header.php
@@ -6,7 +6,7 @@
                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="titlelogo">
                 <img id="logo" src="../image/pixil-layer-Background.png">
                 <a href="../index.php" id="titlelogo">
-                <div id="UwUmargot">CliMatch</div>
+                <div id="CliMatch">CliMatch</div>
             </div>
         </a>
     </div>
