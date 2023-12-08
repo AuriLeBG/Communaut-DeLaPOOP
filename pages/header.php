f5856761ca9b956af68d1b4a865eed938ee6c7c5
Margot <margot.colo@gmail.com>
2023-12-08 03:10:52 +0100

    update journal de bord

--- pages/header.php
+++ pages/header.php
@@ -5,7 +5,7 @@
             <div id="main-title">
                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="titlelogo">
                 <img id="logo" src="../image/pixil-layer-Background.png">
-                <a href="../index.php" id="titlelogo">
+                <a href="https://youtu.be/80hMEKlLVgQ?si=RCnU19NR75gJ6rE4" id="titlelogo">
                 <div id="CliMatch">CliMatch</div>
             </div>
         </a>
