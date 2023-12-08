2eb346e89024c175fb8a352518b4372bca464b3c
unknown <hugoguelton@sfr.fr>
2023-12-08 02:38:07 +0100

    qui les lit vraiment de toute façon

--- javascript/easter_egg.js
+++ javascript/easter_egg.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function(){
     let div = document.getElementById("version_easter_egg");
     div.addEventListener('click', function (evt) {
         if (evt.detail === 3) {
-            window.location.href = "horloge.html";
+            window.location.href = "horloge.php";
         }
     });
 });
